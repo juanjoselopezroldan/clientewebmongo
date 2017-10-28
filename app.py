ec44e92bf58102647472b6f23477d89e427621f4
juan jose lopez roldan <jjlr1994@gmail.com>
2017-10-28 13:50:21 +0200

    modificacion de .py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def resultado():
 	cla = request.forms.get('clave')
 	col = request.forms.get('coleccion')
 
-	uri= "172.22.200.109://"+usu+":"+cla+"/"+col
+	uri= "172.22.200.109: // "+usu+": "+cla+"/"+col
 
 	cliente= MongoClient(uri)
 
