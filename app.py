d3434a07eac90e644bc5b864a925a146f174a9b8
juan jose lopez roldan <jjlr1994@gmail.com>
2017-10-28 12:41:26 +0200

    obtencion de datos del .py

--- app.py
+++ app.py
@@ -3,4 +3,11 @@ from pymongo import MongoClient, GEO2D
 import bottle
 from  bottle import Bottle,route,run,request,template,static_file,default_app,get
 
-@route ('/', metoh)
+@route ('/', method="get" )
+def inicio():
+	return template ('template1.tpl')
+
+@route ('/resultado'method="get")
+def resultado():
+	usu = request.forms.get('usuario')
+	cla = request.forms.get('clave')
